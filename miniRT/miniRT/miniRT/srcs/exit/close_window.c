864815a4448862a956fb9abc31d71444c936af19
Luyao Xu <luyao@luyaos-air.home>
2025-09-02 19:16:48 +0200

    scene img leak fix 2

--- miniRT/miniRT/miniRT/srcs/exit/close_window.c
+++ miniRT/miniRT/miniRT/srcs/exit/close_window.c
@@ -59,20 +59,53 @@ void	destroy_display(void *mlx_ptr)
 //	return (0);
 //}
 
+void	destroy_scene_image(t_scene *scene)
+{
+	t_image	*img;
+	
+	if (!scene)
+		return ;
+	img = scene->img;
+	if (!img)
+		return ;
+	if (scene->mlx_ptr && img->img_ptr)
+		mlx_destroy_image(scene->mlx_ptr, img->img_ptr);
+	free (img);
+	scene->img = NULL;
+}
+
 int	close_program(t_scene *scene, const char *msg, int state)
 {
 	if (msg)
 		ft_putstr_fd((char *)msg, 2);
 	if (scene)
 	{
-		if (scene->mlx_ptr && scene->win_ptr)
-			mlx_destroy_window(scene->mlx_ptr, scene->win_ptr);
+		destroy_scene_image(scene);
 		if (scene->mlx_ptr)
+		{
 			destroy_display(scene->mlx_ptr);
+			scene->mlx_ptr = NULL;
+		}
 		free_scene(scene);
-		free(scene->mlx_ptr);
-		free(scene);
+		free (scene);
 	}
 	exit(state);
-	return (0);
 }
+
+//int	close_program(t_scene *scene, const char *msg, int state)
+//{
+//	if (msg)
+//		ft_putstr_fd((char *)msg, 2);
+//	if (scene)
+//	{
+//		if (scene->mlx_ptr && scene->win_ptr)
+//			mlx_destroy_window(scene->mlx_ptr, scene->win_ptr);
+//		if (scene->mlx_ptr)
+//			destroy_display(scene->mlx_ptr);
+//		free_scene(scene);
+//		free(scene->mlx_ptr);
+//		free(scene);
+//	}
+//	exit(state);
+//	return (0);
+//}
